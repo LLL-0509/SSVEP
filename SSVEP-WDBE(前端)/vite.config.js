f38853088865b58269ab241a25bd88c832fc904f
LLL-0509 <1623197230@qq.com>
2026-03-16 09:56:32 +0800

    Update vite.config.js

--- SSVEP-WDBE(前端)/vite.config.js
+++ SSVEP-WDBE(前端)/vite.config.js
@@ -3,20 +3,21 @@ import fs from 'fs'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/SSVEP_final/',
+
   server: {
     https: {
-      key: fs.readFileSync('public/ssvep_decrypted.key','utf8'),  // 私钥路径
-      cert: fs.readFileSync('public/ssvep.crt','utf8'),  // 证书路径
+      key: fs.readFileSync('public/ssvep_decrypted.key', 'utf8'),
+      cert: fs.readFileSync('public/ssvep.crt', 'utf8'),
       passphrase: 'ssvep'
     },
     proxy: {
       '/api': {
-        target: 'https://localhost:8080',  // 后端地址，使用 HTTPS
+        target: 'https://localhost:8080',
         changeOrigin: true,
-        secure: false,  // 如果是自签名证书，可以设置为 false
-        rewrite: (path) => path.replace(/^\/api/, '')  // 可选：重写路径
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
       },
     },
   },
@@ -24,18 +25,20 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  build:{
-    chunkSizeWarningLimit:1500,
-    emptyOutDir:true,
-    rollupOptions:{
-      output:{
-        manualChunks(id){
-          if(id.includes('node_modules')){
+
+  build: {
+    chunkSizeWarningLimit: 1500,
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
             return id.toString().split('node_modules/')[1].split('/')[0].toString();
           }
         }
